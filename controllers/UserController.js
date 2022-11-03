d35148c2d32cd2580bff524e8ea6b638ea7ed38b
Your Name <you@example.com>
2022-11-03 11:17:50 -0500

    validaciones

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,7 +1,7 @@
 //dependencias
  const sequelize = require ('../config/Seq')
  //data
- const {DataTypes} = require ('sequelize')
+ const {DataTypes, ValidationError} = require ('sequelize')
  //el modelo 
  const UserModel = require ('../models/user')
  //crear la entidad 
@@ -11,7 +11,8 @@
 
 //listar todos los user
 exports.getAllUsers =  async (req , res) =>{
-    //traer los usuarios 
+    try {
+         //traer los usuarios 
     const users = await  User.findAll();
     //responde con los datos 
     res
@@ -20,59 +21,136 @@ exports.getAllUsers =  async (req , res) =>{
         "success": true,
         "data" : users
     })
+    } catch (error) {
+        res
+        .status(400)
+        .json({
+            "sucess":false,
+            "errors":"error de servidor desconocido"
+
+        }) 
+    }
+   
 }
 //Listar user pi Id
 exports.getSingleUser = async (req , res) =>{
-     //traer los usuarios 
+    try {
+        //traer los usuarios 
      const singleUser = await User.findByPk(req.params.id);
-    res
+     if (singleUser){
+        res
     .status(200)
     .json({
         "success": true,
         "data" : singleUser
-    })
+    })  
+     }else{
+        res
+        .status(400)
+        .json({
+            "success": false,
+            "errors":"usuario no existente"
+        })
+     }
+    
+    } catch (error) {
+        res
+        .status(400)
+        .json({
+            "sucess":false,
+            "errors":"error de servidor desconocido"
+
+        }) 
+    }
+   
 }
 
 
 //Actualizar user
 exports.updateUser = async (req , res) =>{
-    await User.update (req.body,{
+     
+    try {
+    const singleUser = await User.findByPk(req.params.id);
+     if(!singleUser){
+        res
+        .status(400)
+        .json({
+            "success": false,
+            "errors":"usuario no existente"
+        })
+     }else{
+        // en caso que actualizo el usuario
+        //console.log(req.params.id)
+        await User.update (req.body,{
         where:{
             id: req.params.id
-        }
-    });
-    const singleUser = await User.findByPk(req.params.id);
-    //console.log(req.params.id)
+        }});
+        //selecciono user actualizado
+        const updateUser = await User.findByPk(req.params.id);
     res
     .status(200)
     .json({
         "success": true,
-        "data" : singleUser
+        "data" : updateUser
     })
+}
+ } catch (error) {
+        res
+        .status(400)
+        .json({
+            "sucess":false,
+            "errors":"error de servidor desconocido"
+
+        }) 
+    }
+    
 }
 
 //Eliminar user
 exports.deleteUser = async (req , res) =>{
-    const singleUser = await User.findByPk(req.params.id);
-    await User.destroy ({
-        where:{
-            id: req.params.id
-        }
-    });
-  
-    res
-    .status(200)
-    .json({
-        "success": true,
-        "data" : singleUser
-    })
+ 
+    try {
+        const singleUser = await User.findByPk(req.params.id);
+         if(!singleUser){
+            res
+            .status(400)
+            .json({
+                "success": false,
+                "errors":"usuario ya no existente"
+            })
+         }else{
+            // en caso que actualizo el usuario
+            //console.log(req.params.id)
+            await User.delete (req.body,{
+            where:{
+                id: req.params.id
+            }});
+            //selecciono user actualizado
+            const deleteUser = await User.findByPk(req.params.id);
+        res
+        .status(200)
+        .json({
+            "success": true,
+            "data" : deleteUser
+        })
+    }
+     } catch (error) {
+            res
+            .status(400)
+            .json({
+                "sucess":false,
+                "errors":"error de servidor desconocido"
+    
+            }) 
+        
+    
 }
 
-
+}
 
 //Crear nuevo user
 exports.createUser = async (req , res) =>{
- 
+ try {
     const newUser = await User.create(req.body);
     res
     .status(200)
@@ -80,6 +158,29 @@ exports.createUser = async (req , res) =>{
         "success": true,
         "data" :newUser
     })
+ } catch (error) {
+    if(error instanceof ValidationError){
+       //recoramos el areglo de errores 
+       //foreach 
+       //map 
+       const errores = error.errors.map((elemento)=>{return elemento.message})
+        res
+        .status(400)
+        .json({
+            "sucess":false,
+            "errors":errores
+        }) 
+    }else{
+        res
+        .status(400)
+        .json({
+            "sucess":false,
+            "errors":"errror de servidor"
+
+        }) 
+    }
+ }
+    
 }
 
 
--- models/user.js
+++ models/user.js
@@ -14,13 +14,60 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User.init({
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate:{
+        unique(value) {  
+          return User.findOne({where:{username:value}})
+            .then((username) => {
+              if (username) {
+                throw new Error('Error hay mas de un nombre asi');
+              }
+            })
+        },
+        isAlpha: {
+          args: true,
+          msg: 'username debe tener solo letras',
+        },
+        notNull: {
+          args: true,
+          msg: 'username debe estar presente'
+        },
+        notEmpty: {
+          args: true,
+          msg: 'username no debe ser vacio'
+        },
+      
+         
+      }
+    },
+    email: {
+      type:DataTypes.STRING,
+      validate:{
+       isEmail:{
+        args: true,
+        msg: 'email no es valido'
+      }, 
+      },
+    },
+
+  password:{
+    type:DataTypes.STRING,
+    validate:{
+      len:{
+        args: [5,10],
+        msg:"password minimo 5 y maximo 10 caracteristicas"
+      },
+    }
+  }
+},{
     sequelize,
     modelName: 'User',
     timestamps: false
   });
   return User;
 };
+
+
